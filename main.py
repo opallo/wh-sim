e764e14329b924785d1fbe4c982a40c5f0ec2278
opallo <opallo.collins@gmail.com>
2024-06-10 01:12:06 -0700

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+  print("Welome to Tiny Warehouse Simulator!")
+  print("1. View Inventory")
+  print("2. Add Item to Inventory")
+  print("3. View Orders")
+  print("4. Add Order")
+  print("Press 'q' to quit")
+  
+if __name__ == "__main__":
+  main()
